7544e935644e194b904e3240eb1cef93a6b37510
danielofir98 <danielofir98@gmail.com>
2025-03-01 06:45:55 +0700

    added console logs

--- server.js
+++ server.js
@@ -70,3 +70,7 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Little Pro server listening on port ${PORT}`);
 });
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+console.log("After server.listen() – no crash yet...");
